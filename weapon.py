0cc2e7a89d6de3c1470fdba24e3ee74628dd21bf
Alesey Kosenkov <starko.web@gmail.com>
2020-02-12 11:17:27 +0300

    weapon

---
+++ weapon.py
@@ -0,0 +1,12 @@
+class Weapon():
+    def set_damage(self, damage):
+        self.damage = damage
+
+    def get_damage(self):
+        return self.damage
+
+    def set_hits(self, hits):
+        self.hits = hits
+
+    def get_hits(self):
+        return self.hits
